a1ca120a1f1b3b04b21ce7a27fffb8affc5dba1a
ThomasVersaevel <Thomas0699@gmail.com>
2022-02-23 11:58:01 +0100

    Added event folder
    - events start

--- index.js
+++ index.js
@@ -11,14 +11,24 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
+// commands
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
-//client.user.setStatus('online'); //added this
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+// events
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
