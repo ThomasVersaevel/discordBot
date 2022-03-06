d40f6e72f09fdcfb961c884ce6e81a3fed97407d
ThomasVersaevel <thomas0699@gmail.com>
2022-03-06 18:22:05 +0100

    Finaly fixed slash commands
    - turns out it was indeed the node version

--- commands/handsome.js
+++ commands/handsome.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageAttachment, MessageEmbed } = require('discord.js');
 
 const pokeArray = ['Shroomish', 'Bidoof', 'Chansey', 'Chimecho', 'Lickitounge', 
     'Ludicolo', 'Metapod', 'Pickachu', 'Psyduck', 'Smoochum', 'Snorelax', 'Wooper'];
@@ -9,33 +9,18 @@ module.exports = {
 		.setName('handsome')
 		.setDescription('Replies with a picture of a handsome Pokémon'),
 	async execute(interaction) {
-
-        await interaction.reply('Handsome');
-
         i = Math.floor( Math.random() * pokeArray.length );
         const imgString = pokeArray[i];
         console.log('Handsome'+imgString+'.png');
-        const pic = new MessageAttachment('../assets/HandsomePokemon/Handsome'+imgString+'.png');
-        // inside a command, event listener, etc.
+        const pic = new MessageAttachment('./assets/HandsomePokemon/Handsome'+imgString+'.png');
+
         const exampleEmbed = new MessageEmbed()
             .setColor('#0099ff')
-            .setTitle('Handsome Pokemon')
-            .setURL('https://discord.js.org/')
-            .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-            .setDescription('Handsome '+imgString)
-            .setThumbnail(pic)
-            .addFields(
-                { name: 'Regular field title', value: 'Some value here' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-            )
-            .addField('Inline field title', 'Some value here', true)
-            .setImage('https://i.imgur.com/AfFp7pu.png')
-            .setTimestamp()
-            .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+            .setTitle('Handsome '+imgString) 
+			.setImage('attachment://poke.png'); //takes attachment from send method below
 
-        channel.send({ embeds: [exampleEmbed] });
-
+        await interaction.channel.send({ embeds: [exampleEmbed], 
+			files: [{ attachment:'assets/HandsomePokemon/Handsome'+imgString+'.png',
+			name:'poke.png'}] });
     },
 };
