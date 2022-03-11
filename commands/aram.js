d842128ecb15c38b715775c6b8e5e70bd4a3afd6
ThomasVersaevel <thomas0699@gmail.com>
2022-03-11 19:06:42 +0100

    Changed aram style
    - kda is now the title
    - shows timestamp in body
    - champion name is in body

--- commands/aram.js
+++ commands/aram.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const { apiKey } = require('../config.json');
 const shortcuts  = require('../api-shortcuts.json');
 const fetch = require('node-fetch');
+const { LolApi, Constants } = require('twisted');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -23,7 +24,7 @@ module.exports = {
             username = shortcuts[id];
         }
         // ## obtain summoner info ##
-        const sumLink = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${apiKey}`
+        const sumLink = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?type=normal&api_key=${apiKey}`
 		const sumResponse = await fetch(sumLink);
         let sumData = await sumResponse.json();
         const puuid = sumData.puuid; // id of user
@@ -31,7 +32,7 @@ module.exports = {
         const matchLink = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?api_key=${apiKey}`
 		const matchIdResponse = await fetch(matchLink);
 		let matchIdData = await matchIdResponse.json();
-        console.log(matchIdResponse);
+        //console.log(matchIdData);
 
 
         // ## From here its the reply ##
@@ -46,36 +47,65 @@ module.exports = {
             .setThumbnail('attachment://icon.png');
    
         var idNr = 1;
+        var startIndex = 0;
         // ## obtain match info from 20 IDs ##
         for (var id = 0; id < matchIdData.length; id++) { //for in uses id as an iterator thus ddo data[id]
             
             let tempLink = `https://europe.api.riotgames.com/lol/match/v5/matches/${matchIdData[id]}?api_key=${apiKey}`
 		    const matchResponse = await fetch(tempLink);
 		    let matchData = await matchResponse.json();
+            //console.log(matchData.info);
             // ## check only for ARAM ##
-            if (matchData.info.gameMode === 'ARAM') {
+            if (matchData.info.gameMode === 'ARAM' && idNr < 22) { //ensure no more than 22 matched displayed
                 //console.log('found aram match');
                 
-
-                var index = 0;
+                // participant index for finding you in each game
+                var partIndex = 0;
                 for (var i = 0; i < 10; i++) {
                     if (matchData.info.participants[i].puuid === puuid) {
-                        index = i;
+                        partIndex = i;
                     }
                 }
-                const partData = matchData.info.participants[index];
+                const partData = matchData.info.participants[partIndex];
 
-                if( matchData.info.participants[index].win ) {
-                    outcome = ':green_circle:';
-                } else {
-                    outcome = ':red_circle:';
+                outcome = matchData.info.participants[partIndex].win ? ':green_circle:' : ':red_circle:';
+
+                let timestampMin = matchData.info.gameDuration / .6;
+                let timestamp = timestampMin.toString();
+                if (timestamp.length < 4) {
+                    timestamp = 0+timestamp; 
                 }
-                // console.log(matchData);
+                let seconds = parseInt(timestamp.substring(2,4))/100 * 60;
+                let secondsString = seconds.toString().replace('.', '0');
+                let stamp = timestamp.substring(0,2)+":"+ secondsString.substring(0,2);
+                let kda = partData.kills+"/"+partData.deaths+"/"+partData.assists;
+
                 exampleEmbed
-                    .addField(outcome+' Match '+ parseInt(idNr++), 
-                    partData.championName + " " + partData.kills+"/"+partData.deaths+"/"+partData.assists, true);
+                    .addField(outcome+" " + " " + kda, 
+                    partData.championName+"  "+stamp, true);
+            }
+            if (idNr < 22 && id == 19) { //method to keep looking for 20 aram games
+                startIndex += 20;
+                if (startIndex < 21) {
+                    id = 0;
+                    // ## obtain 20 more match IDs ##
+                    const matchLink = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=${startIndex}&api_key=${apiKey}`
+                    const matchIdResponse = await fetch(matchLink);
+                    matchIdData = await matchIdResponse.json();
+                    console.log('restarting at '+startIndex);
+                }            
             }
         }
+
+        if ((idNr-1) % 3 == 1) {
+            exampleEmbed
+                .addField('\u200b', '\u200b', true);
+        } else if ((idNr-1) % 3 == 2) {
+            exampleEmbed
+                .addField('\u200b', '\u200b', true)
+                .addField('\u200b', '\u200b', true);
+        }
+
         await interaction.followUp({ embeds: [exampleEmbed], 
             files: [{ attachment: icon,
             name:'icon.png'}] });
