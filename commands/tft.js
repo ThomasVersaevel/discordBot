34ffe313c6b9dc061cb70c0e4500ba7482db7888
ThomasVersaevel <thomas0699@gmail.com>
2022-04-20 16:55:53 +0200

    tftrank command

--- commands/tft.js
+++ commands/tft.js
@@ -34,26 +34,38 @@ module.exports = {
         //console.log(data);
         patchNr = '12.7.1'; //required for data dragon
         let icon = `http://ddragon.leagueoflegends.com/cdn/${patchNr}/img/profileicon/${data.profileIconId}.png`
-
-        let tftlink = `https://europe.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/${puuid}?api_key=${tftKey}`
+        let tftlink = `https://euw1.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/${puuid}?api_key=${tftKey}`
         const tftResponse = await fetch(tftlink);
         let tftData = await tftResponse.json();
+
+        let tftRankedLink = `https://euw1.api.riotgames.com/tft/league/v1/entries/by-summoner/${tftData.id}?api_key=${tftKey}`
+        const tftRankResponse = await fetch(tftRankedLink);
+        let tftRankData = await tftRankResponse.json();
+        let rank = tftRankData[1].tier; //ranked tft
+        rank = rank.substring(0, 1)+ rank.substring(1).toLowerCase();
+        let hyperRank = tftRankData[0].ratedTier; //hyperrol
+        if (hyperRank.toLowerCase() == 'orange') {
+            hyperRank = 'Hyper';
+        }
 
         let tftMatchLink = `https://europe.api.riotgames.com/tft/match/v1/matches/by-puuid/${puuid}/ids?api_key=${tftKey}`
         const tftMatchResponse = await fetch(tftMatchLink);
         let tftMatchData = await tftMatchResponse.json();
-        console.log(tftMatchData);
+        console.log(tftRankData[1]);
 
         var exampleEmbed = new MessageEmbed()
             .setColor('blue')
             .setTitle('')
-            .addField('Level:' , ''+data.summonerLevel, true)
-
+            .addField(''+tftRankData[1].summonerName, '\u200b', true)
+            .addField('Rank: '+rank, '\u200b', false)
+            .addField('Hyperrol rank: '+hyperRank+'tier', '\u200b', false)
+            .setImage('attachment://rankedImg.png')
             .setThumbnail('attachment://icon.png');
 
         await interaction.reply({ embeds: [exampleEmbed], 
-			files: [{ attachment: icon,
-			name:'icon.png'}] });
+			files: [{ attachment: icon, name:'icon.png'},
+             {attachment:'assets/ranked-emblems/emblem_'+ rank +'.png', name:'rankedImg.png'}]
+        });
                       
     },
 };
