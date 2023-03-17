c49d9421c45314c0fdb414af540e8373e44cf8d7
ThomasVersaevel <thomas0699@gmail.com>
2023-03-17 23:20:34 +0100

    Refactored events

---
+++ events/messageCreate.js
@@ -0,0 +1,261 @@
+const { MessageEmbed } = require('discord.js');
+
+/**
+ * On message events
+ */
+module.exports = {
+    name: 'messageCreate',
+    execute(message) {
+
+        const lamaArray = ["laat maar", "laatmaar", "lamaar", "lamar", "lama", "llama"];
+
+        const censorArray = ["kanker", "kkr", "kenker"];
+
+        var wordContainer = "";
+        const pokeArray = [
+            "Shroomish",
+            "Bidoof",
+            "Chansey",
+            "Chimecho",
+            "Lickitounge",
+            "Ludicolo",
+            "Metapod",
+            "Pickachu",
+            "Psyduck",
+            "Smoochum",
+            "Snorelax",
+            "Wooper",
+        ];
+        if (message.content.toLowerCase().includes("annie")) {
+            message.channel.send("Ban ~~annie~~");
+        }
+        if (message.content.toLowerCase().includes("malzahar")) {
+            message.channel.send(`Gadverdakke M\*lz\*h\*r`);
+        }
+        if (message.content.toLowerCase().includes("handsome")) {
+            i = Math.floor(Math.random() * pokeArray.length);
+            const imgString = pokeArray[i];
+            console.log("Handsome" + imgString + ".png");
+            const pic = new MessageAttachment(
+                "../assets/HandsomePokemon/Handsome" + imgString + ".png"
+            );
+            // inside a command, event listener, etc.
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#0099ff")
+                .setTitle("Handsome " + imgString)
+                .setImage("attachment://poke.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/HandsomePokemon/Handsome" + imgString + ".png",
+                        name: "poke.png",
+                    },
+                ],
+            });
+        }
+        if (
+            message.content.toLowerCase().includes("aram") &&
+            (message.content.toLowerCase().includes("tijd") ||
+                message.content.toLowerCase().length < 11)
+        ) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#05AA47")
+                .setImage("attachment://aram.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/AramOetang.png",
+                        name: "aram.png",
+                    },
+                ],
+            });
+        }
+
+        // triggers on any message with tft time/tijd in it except links
+        if (
+            message.content.toLowerCase().includes("tft") &&
+            (message.content.toLowerCase().includes("tijd") ||
+                message.content.toLowerCase().includes("time")) &&
+            !message.content.toLowerCase().includes("/tft/")
+        ) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#05AA47")
+                .setImage("attachment://aram.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/tftea.png",
+                        name: "aram.png",
+                    },
+                ],
+            });
+        }
+        // ### sluipschutters ###
+        if (message.content.toLowerCase().includes("boeie")) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#E50000")
+                .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/SluipSchutters/boeie.png",
+                        name: "sluipschutter.png",
+                    },
+                ],
+            });
+        } else if (message.content.toLowerCase().includes("allebei")) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#E50000")
+                .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/SluipSchutters/allebei.png",
+                        name: "sluipschutter.png",
+                    },
+                ],
+            });
+        } else if (
+            censorArray.some((word) =>
+                message.content.toLowerCase().includes(word.toLowerCase())
+            )
+        ) {
+            var edit = message.content.toLowerCase().split(" ");
+            messageContent = message.content.toLowerCase();
+            for (var i = 0; i < edit.length; i++) {
+                if (censorArray.some((word) => edit[i].includes(word))) {
+                    messageContent = messageContent.replace(edit[i], "####");
+                }
+            }
+            message.delete();
+            message.channel.send(`${message.author.username}: ${messageContent}`);
+        } else if (message.content.toLowerCase().includes("ik dacht dat dat kon")) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#E50000")
+                .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/SluipSchutters/ikdachtdatdatkon.png",
+                        name: "sluipschutter.png",
+                    },
+                ],
+            });
+        } else if (message.content.toLowerCase().includes("moet kunnen")) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#E50000")
+                .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/SluipSchutters/moetkunnen.png",
+                        name: "sluipschutter.png",
+                    },
+                ],
+            });
+        } else if (message.content.toLowerCase().includes("blij ")) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#E50000")
+                .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/SluipSchutters/groteblij.png",
+                        name: "sluipschutter.png",
+                    },
+                ],
+            });
+        }
+        // ### lama ###
+        if (
+            lamaArray.some((word) =>
+                message.content.toLowerCase().includes(word.toLowerCase())
+            )
+        ) {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#E50000")
+                .setImage("attachment://lamazitte.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/lamazitte.png",
+                        name: "lamazitte.png",
+                    },
+                ],
+            });
+        } //joeys neus
+
+        const increasedRandom = {
+            //Returns true if chance is met >0.9 and increases every time it fails.
+            baseChance: 0.1,
+            getRandom: function () {
+                let treshhold = Math.random + this.baseChance;
+                if (treshhold > 0.9) {
+                    return true;
+                } else {
+                    this.baseChance += 0.1;
+                    console.log(treshhold);
+                    return false;
+                }
+            },
+        };
+
+        //&& increasedRandom.getRandom()
+        if (
+            (message.author.id === "183976222215110656" && false) ||
+            message.content.toLowerCase().includes("joeytrigger")
+        ) {
+            // big joey meme
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#E1C699")
+                .setTitle("Grote neus!")
+                .setImage("attachment://neus.png"); //takes attachment from send method below
+
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/neus/neus1.png",
+                        name: "neus.png",
+                    },
+                ],
+            });
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/neus/neus2.png",
+                        name: "neus.png",
+                    },
+                ],
+            });
+            message.channel.send({
+                embeds: [exampleEmbed],
+                files: [
+                    {
+                        attachment: "../assets/neus/neus3.png",
+                        name: "neus.png",
+                    },
+                ],
+            });
+        }
+    },
+};
--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const {
   Client,
   Collection,
   Intents,
-  MessageAttachment,
-  MessageEmbed,
 } = require("discord.js");
-const { token, apiKey } = require("./config.json");
-const fetch = require("node-fetch");
+const { token } = require("./config.json");
 const aramwl = require("./winslosses.json");
-const matchListJson = require("./oldmatchlist.json");
+const tankMeta = require(`./events/tankMeta.js`);
+const aramUpdate = require(`./events/aramUpdate.js`);
 
 // Create a new client instance
 const client = new Client({
@@ -24,8 +22,10 @@ const client = new Client({
   ],
 });
 
+/**
+ * Register commands
+ */
 client.commands = new Collection();
-
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
@@ -37,7 +37,7 @@ for (const file of commandFiles) {
   client.commands.set(command.data.name, command);
 }
 
-// on for bot command interaction
+// Event on interaction for slash commands
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
@@ -56,10 +56,12 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
+/**
+ *  event registration
+*/
 const eventFiles = fs
   .readdirSync(`./events/`)
   .filter((file) => file.endsWith(".js"));
-// event registration
 for (const file of eventFiles) {
   const event = require(`./events/${file}`);
   if (event.once) {
@@ -80,72 +82,11 @@ function loop() {
   // retrieveNewAramGames('Fractaldarkness');
 
   for (entry in aramwl) {
-    // loop over players in aram json file
-    retrieveNewAramGames(entry);
+    // loop over players in aram json file (in the event)
+    aramUpdate.execute(entry);
   }
   setTimeout(loop, 60000 * 60 * 3); // 60000 * 60 * 3 = every 3 hours
 }
-
-async function retrieveNewAramGames(entry) {
-  const link = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${entry}?api_key=${apiKey}`;
-  const response = await fetch(link);
-  sumData = await response.json();
-  const puuid = sumData.puuid; // id of user
-  // obtain 10 aram games by queue 450
-  const matchLink = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?api_key=${apiKey}&queue=450&start=0&count=10`;
-  const matchIdResponse = await fetch(matchLink);
-  let matchIdData = await matchIdResponse.json();
-
-  let oldMatchList = [];
-  //console.log('doing new match logic for: ' + entry);
-
-  for (var i = 0; i < matchListJson[entry].length; i++) {
-    //console.log(match);
-    if (matchListJson[entry][i] != 0) {
-      oldMatchList.push(matchListJson[entry][i]); //list of matchids by sumname (json)
-    }
-  }
-  //console.log(oldMatchList);
-  var win = 0;
-  var lose = 0;
-  let newMatchList = oldMatchList;
-
-  for (var id = 0; id < matchIdData.length; id++) {
-    if (!oldMatchList.includes(matchIdData[id])) {
-      console.log(entry + " New match detected: " + matchIdData[id]);
-      newMatchList.push(matchIdData[id]);
-      let tempLink = `https://europe.api.riotgames.com/lol/match/v5/matches/${matchIdData[id]}?api_key=${apiKey}`;
-      const matchResponse = await fetch(tempLink);
-      let matchData = await matchResponse.json();
-      // if Match not yet used, check win or lose
-      var partIndex = 0;
-      if (!matchData.info) {
-      } else {
-        for (var i = 0; i < 10; i++) {
-          //console.log(matchIdData[id] + ' partIndex: '+ i + ' - ' + matchData.info)
-          if (matchData.info.participants[i].puuid === puuid) {
-            partIndex = i; //find player's index
-            break;
-          }
-        }
-        matchData.info.participants[partIndex].win ? win++ : lose++;
-      }
-    }
-  }
-
-  //console.log(newMatchList);
-  matchListJson[entry] = newMatchList;
-  fs.writeFile("./oldmatchlist.json", JSON.stringify(matchListJson), (err) => {
-    if (err) console.log("Error writing file:", err);
-  });
-  // add wins and losses to total
-  aramwl[entry].wins += win;
-  aramwl[entry].losses += lose;
-  fs.writeFile("./winslosses.json", JSON.stringify(aramwl), (err) => {
-    if (err) console.log("Error writing file:", err);
-  });
-}
-// ####################### end aram updater ##############################
 
 const tank_meta = [
   "tank",
@@ -165,278 +106,16 @@ const tank_meta = [
   "corn",
 ];
 
-const pokeArray = [
-  "Shroomish",
-  "Bidoof",
-  "Chansey",
-  "Chimecho",
-  "Lickitounge",
-  "Ludicolo",
-  "Metapod",
-  "Pickachu",
-  "Psyduck",
-  "Smoochum",
-  "Snorelax",
-  "Wooper",
-];
-
-const lamaArray = ["laat maar", "laatmaar", "lamaar", "lamar", "lama", "llama"];
-
-const censorArray = ["kanker", "kkr", "kenker"];
-
-var wordContainer = "";
-
 client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
   // Tank meta
-  if (
-    tank_meta.some((word) =>
-      message.content.toLowerCase().includes(word.toLowerCase())
-    )
-  ) {
-    const tankMeta = require(`./events/tankMeta.js`);
+  if (tank_meta.some((word) =>
+      message.content.toLowerCase().includes(word.toLowerCase()))) 
+  {
     tankMeta.execute(message); // proper way to call an event
+  } else {
+    messageCreate.execute(message); // call event
   }
-  if (message.content.toLowerCase().includes("annie")) {
-    message.channel.send("Ban ~~annie~~");
-  }
-  if (message.content.toLowerCase().includes("malzahar")) {
-    message.channel.send("Gadverdakke M*lz*h*r");
-  }
-  if (message.content.toLowerCase().includes("handsome")) {
-    i = Math.floor(Math.random() * pokeArray.length);
-    const imgString = pokeArray[i];
-    console.log("Handsome" + imgString + ".png");
-    const pic = new MessageAttachment(
-      "./assets/HandsomePokemon/Handsome" + imgString + ".png"
-    );
-    // inside a command, event listener, etc.
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#0099ff")
-      .setTitle("Handsome " + imgString)
-      .setImage("attachment://poke.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/HandsomePokemon/Handsome" + imgString + ".png",
-          name: "poke.png",
-        },
-      ],
-    });
-  }
-  if (
-    message.content.toLowerCase().includes("aram") &&
-    (message.content.toLowerCase().includes("tijd") ||
-      message.content.toLowerCase().length < 11)
-  ) {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#05AA47")
-      .setImage("attachment://aram.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/AramOetang.png",
-          name: "aram.png",
-        },
-      ],
-    });
-  }
-
-  // triggers on any message with tft time/tijd in it except links
-  if (
-    message.content.toLowerCase().includes("tft") &&
-    (message.content.toLowerCase().includes("tijd") ||
-      message.content.toLowerCase().includes("time")) &&
-    !message.content.toLowerCase().includes("/tft/")
-  ) {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#05AA47")
-      .setImage("attachment://aram.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/tftea.png",
-          name: "aram.png",
-        },
-      ],
-    });
-  }
-  // ### sluipschutters ###
-  if (message.content.toLowerCase().includes("boeie")) {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#E50000")
-      .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/SluipSchutters/boeie.png",
-          name: "sluipschutter.png",
-        },
-      ],
-    });
-  } else if (message.content.toLowerCase().includes("allebei")) {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#E50000")
-      .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/SluipSchutters/allebei.png",
-          name: "sluipschutter.png",
-        },
-      ],
-    });
-  } else if (
-    censorArray.some((word) =>
-      message.content.toLowerCase().includes(word.toLowerCase())
-    )
-  ) {
-    var edit = message.content.toLowerCase().split(" ");
-    messageContent = message.content.toLowerCase();
-    for (var i = 0; i < edit.length; i++) {
-      if (censorArray.some((word) => edit[i].includes(word))) {
-        messageContent = messageContent.replace(edit[i], "####");
-      }
-    }
-    message.delete();
-    message.channel.send(`${message.author.username}: ${messageContent}`);
-  } else if (message.content.toLowerCase().includes("ik dacht dat dat kon")) {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#E50000")
-      .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/SluipSchutters/ikdachtdatdatkon.png",
-          name: "sluipschutter.png",
-        },
-      ],
-    });
-  } else if (message.content.toLowerCase().includes("moet kunnen")) {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#E50000")
-      .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/SluipSchutters/moetkunnen.png",
-          name: "sluipschutter.png",
-        },
-      ],
-    });
-  } else if (message.content.toLowerCase().includes("blij ")) {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#E50000")
-      .setImage("attachment://sluipschutter.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/SluipSchutters/groteblij.png",
-          name: "sluipschutter.png",
-        },
-      ],
-    });
-  }
-  // ### lama ###
-  if (
-    lamaArray.some((word) =>
-      message.content.toLowerCase().includes(word.toLowerCase())
-    )
-  ) {
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#E50000")
-      .setImage("attachment://lamazitte.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/lamazitte.png",
-          name: "lamazitte.png",
-        },
-      ],
-    });
-  } //joeys neus
-
-  const increasedRandom = {
-    //Returns true if chance is met >0.9 and increases every time it fails.
-    baseChance: 0.1,
-    getRandom: function () {
-      let treshhold = Math.random + this.baseChance;
-      if (treshhold > 0.9) {
-        return true;
-      } else {
-        this.baseChance += 0.1;
-        console.log(treshhold);
-        return false;
-      }
-    },
-  };
-
-  //&& increasedRandom.getRandom()
-  if (
-    (message.author.id === "183976222215110656" && false) ||
-    message.content.toLowerCase().includes("joeytrigger")
-  ) {
-    // big joey meme
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#E1C699")
-      .setTitle("Grote neus!")
-      .setImage("attachment://neus.png"); //takes attachment from send method below
-
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/neus/neus1.png",
-          name: "neus.png",
-        },
-      ],
-    });
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/neus/neus2.png",
-          name: "neus.png",
-        },
-      ],
-    });
-    message.channel.send({
-      embeds: [exampleEmbed],
-      files: [
-        {
-          attachment: "assets/neus/neus3.png",
-          name: "neus.png",
-        },
-      ],
-    });
-  } // kevin 263730771917930518 nico 444814987966283777 Thomas 276755182694563850 ThomasDisco 848986609910939668
-  // if (message.author.id === '444814987966283777' || message.content.toLowerCase().includes('nico stil nu')) { // big joey meme
-  // 	//let nico = member.guild.channels.cache.get('848986609910939668');
-  // 	if (message.author.moderatable) {
-  // 		message.author.timeout(60 * 1000, 'sssst');
-  // 		message.channel.send('nico is nu even 1 minuut stil');
-  // 	} else {
-  // 		console.log('cant moderate member: '+message.author.id);
-  // 	}
-  // }
 });
 
 // Login to Discord with your client's token
