040758d9539c3ededb123c1960de71da3b6e7def
ThomasVersaevel <47389450+ThomasVersaevel@users.noreply.github.com>
2023-09-21 19:14:55 +0200

    Changes to arambarplot and ranked.js

--- commands/arambarplot.js
+++ commands/arambarplot.js
@@ -1,156 +1,160 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageAttachment, MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageAttachment, MessageEmbed } = require("discord.js");
 const fs = require("fs");
-const { apiKey } = require('../config.json');
-const shortcuts  = require('../api-shortcuts.json');
-const fetch = require('node-fetch');
-const Canvas = require('canvas');
-const { Chart } = require('chart.js');
-const { Util } = require('util');
-const {convertLolName} = require('../globals.js');
+const { apiKey } = require("../config.json");
+const shortcuts = require("../api-shortcuts.json");
+const fetch = require("node-fetch");
+const Canvas = require("canvas");
+const { Chart } = require("chart.js");
+const { Util } = require("util");
+const { convertLolName } = require("../globals.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-		.setName('arambarplot')
-		.setDescription('Shows your aram wins and loses over the last 20 games in a bar plot')
-        .addStringOption(option =>
-			option.setName('lolname')
-				.setDescription('Summoner Name')
-				.setRequired(true)),
-    async execute(interaction) {
+  data: new SlashCommandBuilder()
+    .setName("arambarplot")
+    .setDescription(
+      "Shows your aram wins and loses over the last 20 games in a bar plot"
+    )
+    .addStringOption((option) =>
+      option
+        .setName("lolname")
+        .setDescription("Summoner Name")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    let username = convertLolName(
+      interaction.options.getString("lolname"),
+      interaction.member.id
+    ); //uses globals
 
-        let username = convertLolName(interaction.options.getString('lolname'), interaction.member.id); //uses globals
+    // ## obtain summoner info ##
+    const sumLink = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?type=normal&api_key=${apiKey}`;
+    const sumResponse = await fetch(sumLink);
+    let sumData = await sumResponse.json();
+    const puuid = sumData.puuid; // id of user
+    //console.log(puuid);
+    // ## obtain 20 match IDs (default) for only aram queue 450##
+    const matchLink = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?api_key=${apiKey}&queue=450`;
+    const matchIdResponse = await fetch(matchLink);
+    let matchIdData = await matchIdResponse.json();
+    //console.log(matchIdData);
 
-        // ## obtain summoner info ##
-        const sumLink = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?type=normal&api_key=${apiKey}`
-		const sumResponse = await fetch(sumLink);
-        let sumData = await sumResponse.json();
-        const puuid = sumData.puuid; // id of user
-        //console.log(puuid);
-        // ## obtain 20 match IDs (default) for only aram queue 450##
-        const matchLink = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?api_key=${apiKey}&queue=450`
-		const matchIdResponse = await fetch(matchLink);
-		let matchIdData = await matchIdResponse.json();
-        //console.log(matchIdData);
+    // ## From here its the reply ##
+    patchNr = shortcuts["patch"];
+    let icon = `http://ddragon.leagueoflegends.com/cdn/${patchNr}/img/profileicon/${sumData.profileIconId}.png`;
+    await interaction.reply(
+      "ARAM wins and losses over the last 20 games for " + username
+    );
 
+    // var exampleEmbed = new MessageEmbed() // empty field .addField('\u200b', '\u200b')
+    //     .setColor('#4e79a7')
+    //     .setTitle(sumData.name)
+    //     .addField('Aram wins and losses', '\u200b', false)
+    //     .setThumbnail('attachment://icon.png')
+    //     .setImage('attachment://canvas.png');
 
-        // ## From here its the reply ##
-        patchNr = shortcuts['patch'];
-        let icon = `http://ddragon.leagueoflegends.com/cdn/${patchNr}/img/profileicon/${sumData.profileIconId}.png`
-        await interaction.reply("ARAM wins and losses over the last 20 games for "+username);
-       
+    var idNr = 1;
+    var startIndex = 0;
 
-        // var exampleEmbed = new MessageEmbed() // empty field .addField('\u200b', '\u200b')
-        //     .setColor('#4e79a7')
-        //     .setTitle(sumData.name)
-        //     .addField('Aram wins and losses', '\u200b', false)
-        //     .setThumbnail('attachment://icon.png')
-        //     .setImage('attachment://canvas.png');
-   
-        var idNr = 1;
-        var startIndex = 0;
+    var wins = 0;
+    var losses = 0;
 
-        var wins = 0;
-        var losses = 0;
+    // ## obtain match info from 20 IDs ##
+    for (var id = 0; id < matchIdData.length; id++) {
+      //for in uses id as an iterator thus ddo data[id]
 
-        // ## obtain match info from 20 IDs ##
-        for (var id = 0; id < matchIdData.length; id++) { //for in uses id as an iterator thus ddo data[id]
-            
-            let tempLink = `https://europe.api.riotgames.com/lol/match/v5/matches/${matchIdData[id]}?api_key=${apiKey}`
-		    const matchResponse = await fetch(tempLink);
-		    let matchData = await matchResponse.json();
-            //console.log(matchData.info);
-            // ## check only for ARAM ##
-                //console.log('found aram match');
-                
-                // participant index for finding you in each game
-                var partIndex = 0;
-                for (var i = 0; i < 10; i++) {
-                    if (matchData.info.participants[i].puuid === puuid) {
-                        partIndex = i;
-                    }
-                }
-                const partData = matchData.info.participants[partIndex];
+      let tempLink = `https://europe.api.riotgames.com/lol/match/v5/matches/${matchIdData[id]}?api_key=${apiKey}`;
+      const matchResponse = await fetch(tempLink);
+      let matchData = await matchResponse.json();
+      //console.log(matchData.info);
+      // ## check only for ARAM ##
+      //console.log('found aram match');
 
-                matchData.info.participants[partIndex].win ? wins++ : losses++;
-
-                
-                idNr++;
+      // participant index for finding you in each game
+      var partIndex = 0;
+      for (var i = 0; i < 10; i++) {
+        if (matchData.info.participants[i].puuid === puuid) {
+          partIndex = i;
         }
+      }
+      const partData = matchData.info.participants[partIndex];
 
-        // chart.js code
-        var winloss = [wins, losses];
-        var labels = ['wins', 'losses'];
+      matchData.info.participants[partIndex].win ? wins++ : losses++;
 
-        const canvas = Canvas.createCanvas(1000, 750);
-		const context = canvas.getContext('2d');
-
-        Chart.defaults.font.size = 36;
-
-        var chart = new Chart(context, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    
-                    data: winloss,
-                    backgroundColor: [
-                        "rgba(75, 192, 192, 0.5)",
-                        "rgba(255, 99, 132, 0.5)"
-                        ],
-                    borderColor: [
-                        "rgba(75, 192, 192, 1)",
-                        "rgba(255, 99, 132, 1)"
-                    ],
-                    borderWidth: 5    
-                }]
-            },
-            options: {
-                plugins: {
-                    legend: {
-                        display: false
-                    },
-                },
-                scales: {
-                    x: {
-                        grid: {
-                            display: true,
-                            drawBorder: true,
-                            drawOnChartArea: true,
-                            drawTicks: true,
-                        },
-                        ticks: {
-                            color: '#ffffff'
-                        }
-                    },
-                    y: {
-                      grid: {
-                        drawBorder: false,
-                        color: function(context) {
-                                if (context.tick.value > 0) {
-                                return '#ffffff';
-                            } else if (context.tick.value < 0) {
-                                return '#ffffff';
-                            } 
-              
-                            return '#ffffff';
-                            },
-                        },
-                      ticks: {
-                        color: '#ffffff'
-                        }
-                    }
-                }
-            }
-        });  
-
-        const background = await Canvas.loadImage('./assets/howlingAbyss.png');
-        const chartImg = await Canvas.loadImage(chart.toBase64Image()); //this works better than JSON.stringify
-        context.drawImage(background, 0, 0, canvas.width, canvas.height);
-	    // Move the image downwards vertically and constrain its height to 200, so that it's square
-	    context.drawImage(chartImg, 0, 0, canvas.width, canvas.height);  
-        const attachment = new MessageAttachment(canvas.toBuffer()); 
-
-        await interaction.editReply({ files: [attachment] });
+      idNr++;
     }
-}
+
+    // chart.js code
+    var winloss = [wins, losses];
+    var labels = ["wins", "losses"];
+
+    const canvas = Canvas.createCanvas(1000, 750);
+    const context = canvas.getContext("2d");
+
+    Chart.defaults.font.size = 36;
+
+    var chart = new Chart(context, {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: winloss,
+            backgroundColor: [
+              "rgba(75, 192, 192, 0.5)",
+              "rgba(255, 99, 132, 0.5)",
+            ],
+            borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
+            borderWidth: 5,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: true,
+              drawBorder: true,
+              drawOnChartArea: true,
+              drawTicks: true,
+            },
+            ticks: {
+              color: "#ffffff",
+            },
+          },
+          y: {
+            grid: {
+              drawBorder: false,
+              color: function (context) {
+                if (context.tick.value > 0) {
+                  return "#ffffff";
+                } else if (context.tick.value < 0) {
+                  return "#ffffff";
+                }
+
+                return "#ffffff";
+              },
+            },
+            ticks: {
+              color: "#ffffff",
+            },
+          },
+        },
+      },
+    });
+
+    const background = await Canvas.loadImage("./assets/howlingAbyss.png");
+    const chartImg = await Canvas.loadImage(chart.toBase64Image()); //this works better than JSON.stringify
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+    // Move the image downwards vertically and constrain its height to 200, so that it's square
+    context.drawImage(chartImg, 0, 0, canvas.width, canvas.height);
+    const attachment = new MessageAttachment(canvas.toBuffer());
+
+    await interaction.editReply({ files: [attachment] });
+  },
+};
---
+++ commands/ranked.js
@@ -0,0 +1,312 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+const shortcuts = require("../api-shortcuts.json");
+const fetch = require("node-fetch");
+const { apiKey } = require("../config.json");
+const { convertLolName } = require("../globals.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ranked")
+    .setDescription("Shows your ranked champion winrates")
+    .addStringOption((option) =>
+      option
+        .setName("lolname")
+        .setDescription("Summoner Name")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    let username = convertLolName(
+      interaction.options.getString("lolname"),
+      interaction.member.id
+    );
+
+    // get User data from lol api
+    const link = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${apiKey}`;
+    const response = await fetch(link);
+    let userData = await response.json();
+    const puuid = userData.puuid;
+
+    patchNr = shortcuts["patch"]; //required for data dragon
+
+    // get Ranked data from lol api
+    let rankedLink = `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${userData.id}?api_key=${apiKey}`;
+    const rankResponse = await fetch(rankedLink);
+    let rankData = await rankResponse.json();
+
+    // Not working looks like nullable in an if statement resolves to true
+    if (!rankData || rankData[0]?.queueType === "RANKED_TFT_DOUBLE_UP") {
+      await interaction.reply({
+        content: "That player does not have any ranked stats",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    let flexRank = "Unranked";
+    let flexDivision = "";
+    let flexLp = 0;
+    let flexWins = 0;
+    let flexLosses = 0;
+
+    let soloRank = "Unranked";
+    let soloDivision = "";
+    let soloLp = 0;
+    let soloWins = 0;
+    let soloLosses = 0;
+
+    let rankedIndex = 100;
+    let flexIndex = 100;
+
+    // console.log(rankData);
+
+    // map each item in rankData and assign the right index to each queue type.
+    // RANKED_SOLO_5x5 RANKED_FLEX_SR CHERRY(arena)
+    for (let i = 0; i < rankData.length; i++) {
+      if (rankData[i].queueType === "RANKED_FLEX_SR") {
+        flexIndex = i;
+      } else if (rankData[i].queueType === "RANKED_SOLO_5x5") {
+        rankedIndex = i;
+      }
+    }
+
+    var chart = new Chart(context, {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: winloss,
+            backgroundColor: [
+              "rgba(75, 192, 192, 0.5)",
+              "rgba(255, 99, 132, 0.5)",
+            ],
+            borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
+            borderWidth: 5,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          x: {
+            grid: {
+              display: true,
+              drawBorder: true,
+              drawOnChartArea: true,
+              drawTicks: true,
+            },
+            ticks: {
+              color: "#ffffff",
+            },
+          },
+          y: {
+            grid: {
+              drawBorder: false,
+              color: function (context) {
+                if (context.tick.value > 0) {
+                  return "#ffffff";
+                } else if (context.tick.value < 0) {
+                  return "#ffffff";
+                }
+
+                return "#ffffff";
+              },
+            },
+            ticks: {
+              color: "#ffffff",
+            },
+          },
+        },
+      },
+    });
+
+    if (rankedIndex < 99) {
+      soloRank = rankData[rankedIndex].tier; //ranked solo
+      soloDivision = rankData[rankedIndex].rank;
+      soloLp = rankData[rankedIndex].leaguePoints;
+      soloWins = rankData[rankedIndex].wins;
+      soloLosses = rankData[rankedIndex].losses;
+    }
+    if (flexIndex < 99) {
+      flexRank = rankData[flexIndex].tier; //ranked flex
+      flexDivision = rankData[flexIndex].rank;
+      flexLp = rankData[flexIndex].leaguePoints;
+      flexWins = rankData[flexIndex].wins;
+      flexLosses = rankData[flexIndex].losses;
+    }
+
+    if (soloRank) {
+      soloRank = soloRank.substring(0, 1) + soloRank.substring(1).toLowerCase();
+    } else {
+      soloRank = "Unranked";
+    }
+
+    if (flexRank) {
+      flexRank = flexRank.substring(0, 1) + flexRank.substring(1).toLowerCase();
+    } else {
+      flexRank = "Unranked";
+    }
+
+    var embedColor = getRankColor(flexRank);
+
+    function getRankIndex(rank) {
+      switch (rank) {
+        case "Unranked":
+          return 0;
+        case "Iron":
+          return 1;
+        case "Bronze":
+          return 2;
+        case "Silver":
+          return 3;
+        case "Gold":
+          return 4;
+        case "Platinum":
+          return 5;
+        case "Emerald":
+          return 6;
+        case "Diamond":
+          return 7;
+        case "Master":
+          return 8;
+        case "Grandmaster":
+          return 9;
+        case "Challenger":
+          return 10;
+        default:
+          return 0;
+      }
+    }
+
+    function getRankColor(rank) {
+      switch (rank) {
+        case "Iron":
+          return "#615959";
+        case "Bronze":
+          return "#925235";
+        case "Silver":
+          return "#839da5";
+        case "Gold":
+          return "#dfa040";
+        case "Platinum":
+          return "#539591";
+        case "Emerald":
+          return "#53bb91";
+        case "Diamond":
+          return "#686cdd";
+        case "Master":
+          return "#8154a6";
+        case "Grandmaster":
+          return "#f12227";
+        case "Challenger":
+          return "#fcf4e1";
+        default:
+          return "#d1d1d1";
+      }
+    }
+
+    let highestRankField = {};
+    let lowestRankField = {};
+    // Find highest rank
+    if (getRankIndex(soloRank) >= getRankIndex(flexRank)) {
+      highestRankField = {
+        name:
+          "Solo Rank: " + soloRank + " " + soloDivision + " " + soloLp + "LP",
+        value:
+          soloWins !== 0 && soloLosses !== 0
+            ? soloWins +
+              " Wins " +
+              soloLosses +
+              " Losses " +
+              Math.round((soloWins / (soloWins + soloLosses)) * 100, 1) +
+              "% WR"
+            : "\u200b",
+        inline: false,
+      };
+      lowestRankField = {
+        name:
+          "Flex Rank: " + flexRank + " " + flexDivision + " " + flexLp + "LP",
+        value:
+          flexWins !== 0 && flexLosses !== 0
+            ? flexWins +
+              " Wins " +
+              flexLosses +
+              " Losses " +
+              Math.round((flexWins / (flexWins + flexLosses)) * 100, 1) +
+              "% WR"
+            : "\u200b",
+        inline: false,
+      };
+    } else {
+      highestRankField = {
+        name:
+          "Flex Rank: " + flexRank + " " + flexDivision + " " + flexLp + "LP",
+        value:
+          flexWins !== 0 && flexLosses !== 0
+            ? flexWins +
+              " Wins " +
+              flexLosses +
+              " Losses " +
+              Math.round((flexWins / (flexWins + flexLosses)) * 100, 1) +
+              "% WR"
+            : "\u200b",
+        inline: false,
+      };
+      lowestRankField = {
+        name:
+          "Solo Rank: " + soloRank + " " + soloDivision + " " + soloLp + "LP",
+        value:
+          soloWins !== 0 && soloLosses !== 0
+            ? soloWins +
+              " Wins " +
+              soloLosses +
+              " Losses " +
+              Math.round((soloWins / (soloWins + soloLosses)) * 100, 1) +
+              "% WR"
+            : "\u200b",
+        inline: false,
+      };
+    }
+
+    var exampleEmbed = new MessageEmbed()
+      .setColor(embedColor)
+      .setTitle("" + userData.name)
+      .addFields(lowestRankField, highestRankField)
+      .setImage("attachment://flex.png")
+      .setThumbnail("attachment://solo.png")
+      .setFooter({
+        text: "Arena wins: " + arenaWins + " losses: " + arenaLosses,
+        iconURL: "attachment://arena.png",
+      });
+
+    await interaction.reply({
+      embeds: [exampleEmbed],
+      files: [
+        { attachment: arenaEmblem, name: "arena.png" },
+        {
+          attachment:
+            "assets/ranked-emblems/Emblem_" +
+            (getRankIndex(flexRank) >= getRankIndex(soloRank)
+              ? flexRank
+              : soloRank) +
+            ".png",
+          name: "flex.png",
+        },
+        {
+          attachment:
+            "assets/ranked-emblems/Emblem_" +
+            (getRankIndex(soloRank) > getRankIndex(flexRank)
+              ? flexRank
+              : soloRank) +
+            ".png",
+          name: "solo.png",
+        },
+      ],
+    });
+  },
+};
